837e6ff500cc5b399cde30f7fef9a8b8b3f461cc
clairedotcom <claireacarson@gmail.com>
2021-04-15 12:54:38 -0400

    calculator functional

--- calculator-script.js
+++ calculator-script.js
@@ -29,7 +29,63 @@ function operate(operation,num1,num2){
     }
 }
 
-console.log(operate("+",2,3));
-console.log(operate("-",3,1));
-console.log(operate("*",9,3));
-console.log(operate("/",4,3));
+//initialize variables and constants used in following functions
+let runningDisplay = "";
+let operationPressed = "";
+const changeDisplay = document.querySelector("#display");
+
+function storeNumKey(e){
+    let displayValue = e.target.id;
+    if (displayValue == "zero"){
+        displayValue = 0;
+    }
+    runningDisplay = runningDisplay + displayValue;
+    updateDisplay(runningDisplay);    
+}
+
+function updateDisplay(value){
+    changeDisplay.textContent = value;
+}
+
+function clear(){
+    runningDisplay = "";
+    updateDisplay(runningDisplay);
+}
+
+function storeOperation(e){
+    operationPressed = e.target.id;
+    previousDisplay = runningDisplay;
+    runningDisplay = "";
+    return operationPressed;
+}
+
+function test(){
+    if (operationPressed == "add"){
+        updateDisplay(operate("+",parseInt(previousDisplay),parseInt(runningDisplay)));
+    }
+    if (operationPressed == "sub"){
+        updateDisplay(operate("-",parseInt(previousDisplay),parseInt(runningDisplay)));
+    }
+    if (operationPressed == "mult"){
+        updateDisplay(operate("*",parseInt(previousDisplay),parseInt(runningDisplay)));
+    }
+    if (operationPressed == "divide"){
+        updateDisplay(operate("/",parseInt(previousDisplay),parseInt(runningDisplay)));
+    }
+}
+
+//add event to number keys
+const numKeys = Array.from(document.querySelectorAll(".number-btn"));
+numKeys.forEach(btn => btn.addEventListener('click', storeNumKey));
+
+//add event to clear key
+const clearDisplay = document.querySelector("#clear");
+clearDisplay.addEventListener("click", clear);
+
+//add event to operation buttons
+const operationBtn = Array.from(document.querySelectorAll(".operation-btn"));
+operationBtn.forEach(btn => btn.addEventListener("click", storeOperation));
+
+//add event to equals button
+const equalsBtn = document.querySelector("#equals");
+equalsBtn.addEventListener("click", test);
